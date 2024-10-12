69f8461a4fc073c0493ae3ec75eb33e842881999
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-10-12 17:31:17 -0400

    Update DinoBash.c
    Almost everything including pipes. Need to add comments, color, and revise.

--- Homework3/DinoBash.c
+++ Homework3/DinoBash.c
@@ -1,7 +1,7 @@
 /*
  Author: Jaleel Rogers
  Class:
- Date: 
+ Date:
  */
 // C libraries
 #include<stdio.h>
@@ -93,13 +93,115 @@ int cmdHandler(char **parsed) {
     return 0;
 }
 
-void execArgsPiped(char** parsed, char** parsedpipe);
-int parsePipe(char* str, char** strpiped);
-void parseSpace(char* str, char** parsed);
-int processString(char* str, char** parsed, char** parsedpipe);
+void execArgsPiped(char** parsed, char** parsedPipe) {
+    int pipe[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipe) < 0) {
+        printf("\nPipe failed.\n");
+        return;
+    }
+    pid1 = fork();
+    if (pid1 < 0) {
+        printf("\nFork failed.\n");
+        return;
+    }
+    if (pid1 == 0) {
+        close(pipe[0]);
+        dup2(pipe[1], STDOUT_FILENO);
+        close(pipe[1]);
+
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nExec failed.\n");
+            exit(0);
+        }
+    }
+        else {
+            pid2 = fork();
+
+            if (pid2 < 0) {
+                printf("\nFork failed.\n");
+                return;
+            }
+
+            if (pid2 == 0) {
+                close(pipe[1]);
+                dup2(pipe[0], STDIN_FILENO);
+                close(pipe[0]);
+                if (execvp(parsedPipe[0], parsedPipe) < 0) {
+                    printf("\nExec failed.\n");
+                    exit(0);
+                }
+            }
+            else {
+                wait(NULL);
+                wait(NULL);
+            }
+        }
+    }
+int parsePipe(char* str, char** strpiped) {
+    int i;
+
+    for (i = 0; i < 2; i++) {
+        strpiped[i] = strsep(&str, "|");
+        if (strpiped[i] == NULL)
+            break;
+    }
+
+        if (strpiped[1] == NULL)
+            return 0;
+        else {
+            return 1;
+        }
+}
+void parseSpace(char* str, char** parsed) {
+    int i;
+
+    for (i = 0; i < MAXLIST; i++) {
+        parsed[i] = strsep(&str, " ");
+
+        if (parsed[i] == NULL)
+            break;
+        if (strlen(parsed[i]) == 0)
+            i--;
+    }
+}
+
+int processString(char* str, char** parsed, char** parsedPipe) {
+    char* strpiped[2];
+    int piped = 0;
+
+    piped = parsePipe(str, strpiped);
+
+    if (piped) {
+        parseSpace(strpiped[0], parsed);
+        parseSpace(strpiped[1], parsedPipe);
+    }
+    else {
+        parseSpace(str, parsed);
+    }
+    if (cmdHandler(parsed))
+        return 0;
+    else
+        return 1 + piped;
+}
 
 
 int main(void) {
     motd();
+    char inputString[MAXCOM], *parsedArgs[MAXLIST];
+    char* parsedArgsPiped[MAXLIST];
+    int execFlag = 0;
+    init_shell();
+
+    while (1) {
+        execFlag = processString(inputString, parsedArgs, &parsedArgsPiped);
+
+        if (execFlag == 1)
+            execArgs(parsedArgs);
+
+        if (execFlag == 2)
+            execArgsPiped(parsedArgs, parsedArgsPiped);
+    }
     return 0;
 }
