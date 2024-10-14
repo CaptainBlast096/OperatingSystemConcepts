ca7efd29f2e36568a02f6ea8061a1e6257f75028
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-10-14 01:07:36 -0400

    Update DinoBash.c
    Finished rough draft. Import to Ubuntu Server for debugging.

--- Homework3/DinoBash.c
+++ Homework3/DinoBash.c
@@ -1,23 +1,27 @@
 /*
  Author: Jaleel Rogers
- Class:
- Date:
+ Class: COP4610.04
+ Date: 10/23/2024
  */
-// C libraries
-#include<stdio.h>
-#include <stdlib.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-//#include<sys/wait.h>
-//#include<readline/readline.h>
-//#include<readline/history.h>
 
-// Define commands
+//C libraries
+#include<stdio.h> //Basic input and output
+#include<stdlib.h> //Using for exit
+#include<string.h> //Manipulating strings
+#include<unistd.h> //Provides access to POSIX
+#include<sys/types.h> //Functionality of pid_t
+#include<sys/wait.h> //Functionality of wait
+#include<readline/readline.h> //Functionality of readline
+#include<readline/history.h> //Functionality of history releated functions
+#include<curses.h> //Provides a variety of colors
 
+//Define commands
+#define MAXCOM 1000 //Max number of letters supported
+#define MAXLIST 100 //Max number of commands supported
+
+//Startup greeting
 void motd() {
-    //printf("\033[0;34m"); supposed to change color
+    attron(COLOR_PAIR(2)); //Sets the current foreground color
     printf("\n");
     printf("  Wecome to Dino Shell!\n");
     printf("-------------------------------\n");
@@ -26,12 +30,17 @@ void motd() {
     char* username = getenv("USER");
     printf("USER is: @%s", username);
     printf("\n");
+    attroff(COLOR_PAIR(2)); // Resets the color attributes to default values
 }
 
+//Reads a line of input from the user and stores it in a buffer along with history
 int userInput(char *str) {
     char* buffer;
+    attron(COLOR_PAIR(1));
+    buffer = readline("\n>>>"); //readline reads a line of input
+    attroff(COLOR_PAIR(1));
 
-    buffer = readline("\n>>>");
+    // Checking user input, if its not empty
     if (strlen(buffer) != 0) {
         add_history(buffer);
         strcpy(str, buffer);
@@ -41,36 +50,47 @@ int userInput(char *str) {
         return 1;
     }
 }
-void execArgs(char **parsed) {
-    pid_t pid = fork();
 
-    if (pid == -1) {
+//System command is executed
+void execArgs(char **parsed) {
+    pid_t pid = fork(); //Forks a child
+    attron(COLOR_PAIR(2));
+
+    if (pid == -1) { //In case there is an error
         printf("\nFailed forking child...");
         return;
     }
-    else if (pid == 0) {
-        if (execvp(parsed[0], parsed) < 0) {
+    else if (pid == 0) { //Current child process
+        if (execvp(parsed[0], parsed) < 0) { //execvp executes the command
             printf("\nCould not execute command...");
         }
-        exit(0);
-    } else {
-        wait(NULL);
+        exit(0); //In case execvp fails
+    } else { //Parent process, indicates the starting and stopping of process
+        printf("--------------------Starting program --------------------------\n");
+        wait(NULL); // Wait for the child to finish
+        printf("__________________Program ended --------------\n");
         return;
     }
+    attroff(COLOR_PAIR(2));
 }
 
+//Don't think this is necessary, chdir() is a built-in function within unistd.h
 int cd(char *path) {
+    attron(COLOR_PAIR(2));
+
     if (cd(path) != 0) {
         perror("chdir"); //Prints and error message
         return 1;
     }
     return 0;
+    attroff(COLOR_PAIR(1));
 }
 
-int cmdHandler(char **parsed) {
-    int numCmds = 2, i, switchFlag;
-    char* username, commandList[numCmds];
-
+//Executes builtin commands
+int cmdHandler(char** parsed) {
+    int numCmds = 2, i, switchFlag = 0;
+    char* commandList[numCmds];
+    attron(COLOR_PAIR(2));
     commandList[0] = "exit";
     commandList[1] = "cd";
 
@@ -80,128 +100,154 @@ int cmdHandler(char **parsed) {
             break;
         }
     }
+
     switch (switchFlag) {
         case 1:
             printf("\nExiting Dino Shell.\n");
             exit(0);
         case 2:
-            cd(parsed[1]);
+            chdir(parsed[1]);
             return 1;
         default:
             break;
     }
     return 0;
+    attroff(COLOR_PAIR(2));
 }
 
+//Executes the pipe
 void execArgsPiped(char** parsed, char** parsedPipe) {
-    int pipe[2];
-    pid_t pid1, pid2;
+    int pipe_fd[2];
+    pid_t pid1, pid2; //Two procceses
+    attron(COLOR_PAIR(2));
 
-    if (pipe(pipe) < 0) {
+    if (pipe(pipe_fd) < 0) { //Creates a pipe via systemcall
         printf("\nPipe failed.\n");
         return;
     }
-    pid1 = fork();
-    if (pid1 < 0) {
+
+    pid1 = fork(); //First child fork
+    if (pid1 < 0) {//In case the first fork fails
         printf("\nFork failed.\n");
         return;
     }
-    if (pid1 == 0) {
-        close(pipe[0]);
-        dup2(pipe[1], STDOUT_FILENO);
-        close(pipe[1]);
 
-        if (execvp(parsed[0], parsed) < 0) {
+    if (pid1 == 0) {//Close the read end of pipe if its the first child
+        close(pipe_fd[0]);
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[1]);
+
+        if (execvp(parsed[0], parsed) < 0) {//In case execution fails
             printf("\nExec failed.\n");
             exit(0);
         }
     }
-        else {
+        else {//If its the parent
             pid2 = fork();
 
-            if (pid2 < 0) {
+            if (pid2 < 0) {//In case the second fork fails
                 printf("\nFork failed.\n");
                 return;
             }
 
-            if (pid2 == 0) {
-                close(pipe[1]);
-                dup2(pipe[0], STDIN_FILENO);
-                close(pipe[0]);
-                if (execvp(parsedPipe[0], parsedPipe) < 0) {
+            if (pid2 == 0) {//Closes the write end of the pipe
+                close(pipe_fd[1]);
+                dup2(pipe_fd[0], STDIN_FILENO);
+                close(pipe_fd[0]);
+
+                if (execvp(parsedPipe[0], parsedPipe) < 0) {//In case execution fails
                     printf("\nExec failed.\n");
                     exit(0);
                 }
             }
-            else {
+            else {//Each wait is for a child
                 wait(NULL);
                 wait(NULL);
             }
         }
+    attroff(COLOR_PAIR(2));
     }
+
+//Finds then parses the pipe
 int parsePipe(char* str, char** strpiped) {
     int i;
+    attron(COLOR_PAIR(2));
 
     for (i = 0; i < 2; i++) {
-        strpiped[i] = strsep(&str, "|");
-        if (strpiped[i] == NULL)
+        strpiped[i] = strsep(&str, "|"); //Split string into substrings
+        if (strpiped[i] == NULL) //When no more substrings need to be parsed
             break;
     }
 
-        if (strpiped[1] == NULL)
+        if (strpiped[1] == NULL) //If only one substring was found
             return 0;
         else {
             return 1;
         }
+    attroff(COLOR_PAIR(2));
 }
+
+//Parses command words into two words
 void parseSpace(char* str, char** parsed) {
     int i;
 
     for (i = 0; i < MAXLIST; i++) {
-        parsed[i] = strsep(&str, " ");
+        parsed[i] = strsep(&str, " "); //Split the string into substrings
 
-        if (parsed[i] == NULL)
+        if (parsed[i] == NULL) //When there are no more substrings
             break;
-        if (strlen(parsed[i]) == 0)
+
+        if (strlen(parsed[i]) == 0) //When parsed substring is empty
             i--;
     }
 }
 
+//Proccess a given string and parsing it into arguments for execution
 int processString(char* str, char** parsed, char** parsedPipe) {
     char* strpiped[2];
     int piped = 0;
 
-    piped = parsePipe(str, strpiped);
+    piped = parsePipe(str, strpiped); //Checks if the input string contains a pipe character
 
     if (piped) {
-        parseSpace(strpiped[0], parsed);
-        parseSpace(strpiped[1], parsedPipe);
+        parseSpace(strpiped[0], parsed); //No pipe was found
+        parseSpace(strpiped[1], parsedPipe); //Pipe was found
     }
+
     else {
-        parseSpace(str, parsed);
+        parseSpace(str, parsed); //Parses the entire input string
     }
-    if (cmdHandler(parsed))
+
+    if (cmdHandler(parsed)) //Calls cmdHandler to handle passed arguments
         return 0;
+
     else
         return 1 + piped;
 }
 
-
 int main(void) {
+    //Curses libraries may not be needed
+    initscr(); //Initializes the curses library
+    noecho(); //Disables echoing of typed characters to terminal
+    cbreak(); //Sets the terminal to raw mode, characters are read immeidately without line buffering
+    start_color(); //Enables color support in the terminal
+    init_pair(1, COLOR_WHITE, COLOR_BLACK); // User color
+    init_pair(2, COLOR_GREEN, COLOR_BLACK); //Program color
     motd();
     char inputString[MAXCOM], *parsedArgs[MAXLIST];
     char* parsedArgsPiped[MAXLIST];
     int execFlag = 0;
-    init_shell();
 
     while (1) {
         execFlag = processString(inputString, parsedArgs, &parsedArgsPiped);
+            //Calls the processString function to parse the user's input and determines
 
-        if (execFlag == 1)
+        if (execFlag == 1) //If the input is a simple command
             execArgs(parsedArgs);
 
-        if (execFlag == 2)
+        if (execFlag == 2) //If the input is a piped command
             execArgsPiped(parsedArgs, parsedArgsPiped);
     }
+    endwin(); //Ends curses mode and restores the terminal to its orginal state
     return 0;
 }
