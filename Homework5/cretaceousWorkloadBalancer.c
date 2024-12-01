2217823af6ffb7c4269019c37463f22e06ffc374
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-12-01 11:08:50 -0500

    Update cretaceousWorkloadBalancer.c

--- Homework5/cretaceousWorkloadBalancer.c
+++ Homework5/cretaceousWorkloadBalancer.c
@@ -6,18 +6,54 @@
 #include <pthread.h>
 
 #define NUM_THREADS 5
+
 typedef struct {
     int work;
     char ID;
 } ThreadData;
 
+sem_t semaphores[NUM_SEMAPHORES];
+
+void random_sleep() {
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = (rand() % 11) * 1000000;
+    nanosleep(&ts, NULL);
+}
 void* Thread(void* arg) {
     ThreadData* data = (ThreadData*)arg;
 
+    // Seeding the random number generator
     unsigned int seed = time(NULL) ^ (pthread_self() & 0xFFFFFF);
 
     while (data -> work > 0) {
-        printf("Thread %c: Work remaining = %d\n", data -> ID, data-> work);
+        // Randomly decide the number of semaphores needed (1 to 3)
+        int pick_sem = (rand_r(&seed) % NUM_SEMAPHORES) + 1;
+
+        int selected[NUM_SEMAPHORES] = {0};
+        for (int i = 0; i < pick_sem; i++) {
+            int sem;
+            do {
+                sem = rand_r(&seed) % NUM_SEMAPHORES;
+            } while (selected[sem]); // Helps with randomness
+            selected[sem] = 1;
+        }
+        int acquired_flag = 1;
+        for (int i = 0; i < NUM_SEMAPHORES; i++) {
+            if (selected[i] && sem_trywait(&semaphores[i]) != 0) {
+                acquired_all = 0;
+                break;
+            }
+        }
+        if (acquired_flag) {
+            for (int i = 0; i < NUM_SEMAPHORES; i++) {
+                if (selected[i]) {
+                    printf("%c>%d\n,", data->ID, i);
+                }
+            }
+            printf(" has %d work left\n")
+        }
+       // printf("Thread %c: Work remaining = %d\n", data -> ID, data-> work);
         sleep(1);
         data -> work--;
     }
@@ -30,6 +66,7 @@ void* Thread(void* arg) {
  * Create an exit once work is 0
  */
 int main() {
+    sem_t sem1, sem2, sem3;
     pthread_t threads[NUM_THREADS];
     ThreadData threadData[NUM_THREADS];
     int result;
