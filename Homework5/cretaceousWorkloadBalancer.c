8c93659a37e2041790576eba876cac48de6a2f54
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2024-12-01 12:02:55 -0500

    Update cretaceousWorkloadBalancer.c
    Phase 2 prototyping

--- Homework5/cretaceousWorkloadBalancer.c
+++ Homework5/cretaceousWorkloadBalancer.c
@@ -1,3 +1,8 @@
+/*
+ * Author: Jaleel Rogers
+ * Title: CretaceousWorkloadBalancer
+ * Date: 12/06/24
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,73 +11,104 @@
 #include <pthread.h>
 
 #define NUM_THREADS 5
+#define NUM_SEMAPHORES 3
 
 typedef struct {
     int work;
     char ID;
 } ThreadData;
 
-sem_t semaphores[NUM_SEMAPHORES];
+sem_t semaphores[NUM_SEMAPHORES]; // Binary semaphores
 
+// Function to sleep for a random amount of time (0-10ms)
 void random_sleep() {
     struct timespec ts;
     ts.tv_sec = 0;
-    ts.tv_nsec = (rand() % 11) * 1000000;
+    ts.tv_nsec = (rand() % 11) * 1000000; // 0-10ms in nanoseconds
     nanosleep(&ts, NULL);
 }
+
 void* Thread(void* arg) {
     ThreadData* data = (ThreadData*)arg;
 
-    // Seeding the random number generator
+    // Seed random number generator for each thread
     unsigned int seed = time(NULL) ^ (pthread_self() & 0xFFFFFF);
 
-    while (data -> work > 0) {
+    while (data->work > 0) {
         // Randomly decide the number of semaphores needed (1 to 3)
-        int pick_sem = (rand_r(&seed) % NUM_SEMAPHORES) + 1;
+        int sem_needed = (rand_r(&seed) % NUM_SEMAPHORES) + 1;
 
+        // Randomly select `sem_needed` semaphores
         int selected[NUM_SEMAPHORES] = {0};
-        for (int i = 0; i < pick_sem; i++) {
+        for (int i = 0; i < sem_needed; i++) {
             int sem;
             do {
                 sem = rand_r(&seed) % NUM_SEMAPHORES;
-            } while (selected[sem]); // Helps with randomness
+            } while (selected[sem]); // Ensure uniqueness
             selected[sem] = 1;
         }
-        int acquired_flag = 1;
+
+        // Try to lock the selected semaphores
+        int acquired_all = 1;
         for (int i = 0; i < NUM_SEMAPHORES; i++) {
             if (selected[i] && sem_trywait(&semaphores[i]) != 0) {
-                acquired_all = 0;
+                acquired_all = 0; // Could not acquire one of the semaphores
                 break;
             }
         }
-        if (acquired_flag) {
+
+        if (acquired_all) {
+            // Successfully acquired all required semaphores
             for (int i = 0; i < NUM_SEMAPHORES; i++) {
                 if (selected[i]) {
-                    printf("%c>%d\n,", data->ID, i);
+                    printf("%c>%d\n", data->ID, i); // Print acquisition
                 }
             }
-            printf(" has %d work left\n")
+            printf("%c has %d work left\n", data->ID, data->work - 1);
+            data->work--; // Reduce work
+
+            // Release semaphores
+            for (int i = 0; i < NUM_SEMAPHORES; i++) {
+                if (selected[i]) {
+                    printf("%c<%d\n", data->ID, i); // Print release
+                    sem_post(&semaphores[i]);
+                }
+            }
+
+            // Sleep for a random amount of time
+            random_sleep();
+        } else {
+            // Could not acquire all semaphores, release any that were held
+            for (int i = 0; i < NUM_SEMAPHORES; i++) {
+                if (selected[i]) {
+                    printf("%c<%d\n", data->ID, i); // Print release
+                    sem_post(&semaphores[i]);
+                }
+            }
+
+            // Sleep for a random amount of time
+            random_sleep();
         }
-       // printf("Thread %c: Work remaining = %d\n", data -> ID, data-> work);
-        sleep(1);
-        data -> work--;
     }
-    printf("Thread %c: Finished all work, exiting.\n", data -> ID);
+
+    printf("Thread %c: Finished all work, exiting.\n", data->ID);
     pthread_exit(NULL);
 }
-/*
- * Need to find a way to create four version of the thread
- * Assign a work value to each integer
- * Create an exit once work is 0
- */
+
 int main() {
-    sem_t sem1, sem2, sem3;
     pthread_t threads[NUM_THREADS];
     ThreadData threadData[NUM_THREADS];
     int result;
 
+    // Initialize semaphores
+    for (int i = 0; i < NUM_SEMAPHORES; i++) {
+        sem_init(&semaphores[i], 0, 1); // Binary semaphore
+    }
+
+    // Seed random number generator
     srand(time(NULL));
 
+    // Create threads with unique IDs and initial work values
     for (int i = 0; i < NUM_THREADS; i++) {
         threadData[i].work = 10;
         threadData[i].ID = 'A' + i;
@@ -84,12 +120,16 @@ int main() {
         }
     }
 
+    // Wait for all threads to finish
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i],NULL);
+        pthread_join(threads[i], NULL);
+    }
+
+    // Destroy semaphores
+    for (int i = 0; i < NUM_SEMAPHORES; i++) {
+        sem_destroy(&semaphores[i]);
     }
 
     printf("Main thread: All threads have finished.\n");
-
-
     return 0;
 }
